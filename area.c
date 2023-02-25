8fa43c87ec0212a7e932aff482095bcf1b189aa3
satyam-thakur029 <thakursatyam029@gmail.com>
2023-02-25 20:24:22 +0530

    first commit

---
+++ area.c
@@ -0,0 +1,47 @@
+// #include<stdio.h>
+// #include<conio.h>
+// float rec (float a ,float b);
+// float squ ( float a);
+// float cir ( float a);
+// int main()
+// {  float a,b;
+// printf("enter a number 1:");
+// scanf("%f",&a);
+// printf("enter a number 2:");
+// scanf("%f",&b);
+// printf("%f\n",rec(a,b));
+//  printf("%f\n",squ(a));
+// printf("%f\n",cir(a));
+// return 0;
+
+// }float rec (float a, float b)
+// {
+//     return a*b;
+// }float squ( float a){
+//     return a*a;
+// }float cir(float a){
+//     return 3.14*a*a;
+// }
+// #include<stdio.h>
+// #include<conio.h>
+// float abc(float a ,float b);
+// float boss(float a);
+// float cut(float b);
+
+// int main(){
+//     float a=88;
+//     float b=90;
+// printf("%f\n",abc(a,b));
+// printf("%f\n", boss(a));
+// printf("%f\n", cut (b));
+
+//     return 0;
+// }float abc(float a ,float b){
+//     return a*b;
+// }
+// float boss(float a){
+//     return 3.14*a*a;
+// }
+// float cut(float b){
+// return b*b;
+// }
---
+++ average.c
@@ -0,0 +1,17 @@
+#include<stdio.h.>
+#include<conio.h>
+int main()
+{ float a,b,c,sum,average;
+printf("enter a first number a:\n");
+scanf("%f",&a);
+printf("enter a second number b:\n");
+scanf("%f",&b);
+printf("enter a third number c:\n");
+scanf("%f",&c);
+sum=a+b+c;
+printf("total sum%f",sum);
+average=((sum)/3);
+printf("print average number %f",average);
+getch();
+   
+   }
